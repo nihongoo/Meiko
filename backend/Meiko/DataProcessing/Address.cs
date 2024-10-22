1f9c9927cba1ca34a651a0333c329cc2f969ae24
Lê Đức Cường <138002821+SayHrYera@users.noreply.github.com>
2024-10-22 13:43:57 +0700

    Squashed commit of the following:
    commit 802402b305776667d2783d3abea405879f51d4e0
    Author: Lê Đức Cường <138002821+SayHrYera@users.noreply.github.com>
    Date:   Tue Oct 22 13:41:35 2024 +0700
    
        Add Models, chưa config

---
+++ backend/Meiko/DataProcessing/Address.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessing
+{
+    public class Address
+    {
+        [Key]
+        [Required(ErrorMessage = "Id không được để trống.")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Tên người nhận không được để trống.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Tên người nhận phải từ 3 đến 100 ký tự.")]
+        public string RecipientName { get; set; }
+
+        [Required(ErrorMessage = "Số điện thoại không được để trống.")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Địa chỉ chi tiết không được để trống.")]
+        [StringLength(200, ErrorMessage = "Địa chỉ chi tiết không được vượt quá 200 ký tự.")]
+        public string AddressDetail { get; set; }
+
+        [Required(ErrorMessage = "Thành phố không được để trống.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Quận/Huyện không được để trống.")]
+        public string District { get; set; }
+
+        [Required(ErrorMessage = "Phường/Xã không được để trống.")]
+        public string Ward { get; set; }
+
+        [Required(ErrorMessage = "Trạng thái không được để trống.")]
+        [Range(0, 5, ErrorMessage = "Trạng thái phải từ 0 đến 5.")]
+        public int Status { get; set; }
+
+        public Guid? CustomerId { get; set; }
+        public virtual Customers? Customers { get; set; }
+    }
+}
