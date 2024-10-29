3bbac77e1dd2a67f1ebddc243357539de6c4cb5b
Lê Đức Cường <138002821+SayHrYera@users.noreply.github.com>
2024-10-29 15:02:21 +0700

    Update index.js

--- frontend/meiko/src/routes/API/index.js
+++ frontend/meiko/src/routes/API/index.js
@@ -7,6 +7,14 @@ const apiURL = {
         delete: 'https://localhost:7265/Category/Delete-Category',
         search: 'https://localhost:7265/Category/Search'
     },
+    account:{
+        base: 'https://localhost:7172/api/Account/Account/',
+        create_customer: 'https://localhost:7172/api/Account/register-customer/',
+        create_staff: 'https://localhost:7172/api/Account/register-staff/',
+        login: 'https://localhost:7172/api/Account/login/',
+        forgot_password: 'https://localhost:7172/api/Account/forgot-password/',
+        verify_otp: 'https://localhost:7172/api/Account/verify-otp/'
+    }
 }
 
 export default apiURL
