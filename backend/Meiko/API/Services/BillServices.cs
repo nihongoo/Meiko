e35d1c8d24146032bf771202292fb3a569ee5dda
giaptd1610 <giaptd.work@gmail.com>
2024-12-12 18:46:57 +0700

    Sửa lỗi không cập nhật trạng thái đơn hàng

--- backend/Meiko/API/Services/BillServices.cs
+++ backend/Meiko/API/Services/BillServices.cs
@@ -963,7 +963,7 @@ namespace API.Services
 
 
 		//Payment History
-		public async Task<ReturnMessage> Pay(decimal AmountInput, int PaymentMethod, int Status, Guid BillId)
+		public async Task<ReturnMessage> Pay(decimal AmountInput, int PaymentMethod, int Status, Guid BillId, Guid? WhoDoThis)
 		{
 			using (var dbTransaction = await _dbcontext.Database.BeginTransactionAsync())
 			{
@@ -986,6 +986,12 @@ namespace API.Services
 							status = 4,
 							message = "Đơn hàng đã bị huỷ, không thể thanh toán"
 						};
+					if (WhoDoThis == null)
+						return new ReturnMessage()
+						{
+							status = 4,
+							message = "Không tìm thấy người thực hiên hành dộng"
+						};
 					await _dbcontext.PaymentHistories.AddAsync(new PaymentHistory()
 					{
 						Id = Guid.NewGuid(),
@@ -993,6 +999,15 @@ namespace API.Services
 						Amount = AmountInput,
 						PaymentMethod = (PaymentMethods)PaymentMethod,
 						Status = (StatusForPayment)Status,
+						BillId = BillId
+					});
+
+					await _dbcontext.StatusHistories.AddAsync(new StatusHistory()
+					{
+						Id = Guid.NewGuid(),
+						CreatedDate = DateTime.Now,
+						StatusType = StatusType.DaThanhToan,
+						WhoCreatedThis = (Guid)WhoDoThis,
 						BillId = BillId
 					});
 
@@ -1015,6 +1030,7 @@ namespace API.Services
 
 					bill.PaymentDate = DateTime.Now;
 					bill.PaymentAmount = AmountInput;
+					bill.Status = StatusType.DaThanhToan;
 
 					_dbcontext.Bills.Update(bill);
 
